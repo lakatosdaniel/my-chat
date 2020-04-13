7629865f10874858c79b0d98703537c1caf06c88
Daniel Lakatos <lakatos.dani96@gmail.com>
2020-04-13 12:10:59 +0200

    adding build

---
+++ build/precache-manifest.a806da07104b1c01562d5e10e760e509.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7965a4001ae24be662471eb125fad7d1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9d8c8e4c3b15516ff8d2",
+    "url": "./static/css/main.70bf84e9.chunk.css"
+  },
+  {
+    "revision": "894daed5cac13c915747",
+    "url": "./static/js/2.ee16ff66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ee16ff66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d8c8e4c3b15516ff8d2",
+    "url": "./static/js/main.a7c8372f.chunk.js"
+  },
+  {
+    "revision": "821e3e9e8623f25acdce",
+    "url": "./static/js/runtime-main.62724d6a.js"
+  }
+]);
