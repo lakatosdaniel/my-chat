465825c6068d05b5d7a0b9cf6beac6e7e8a7027e
Daniel Lakatos <lakatos.dani96@gmail.com>
2020-04-13 12:02:23 +0200

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Login } from './Login'
 import './App.css';
+import { Main } from './Main';
+import { proxy } from './Proxy';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    if (proxy.inbox != null) {
+      return (<div className="app"><Main /></div>); 
+    }
+    else {
+      return (<div className="app"><Login /></div>); 
+    }
+  }
+  componentDidMount() {
+    proxy.addEventListener( "login", () => this.forceUpdate(), this ); 
+  }
+  componentWillUnmount() {
+      proxy.removeAllEventListener( this ); 
+  }
 }
-
-export default App;
